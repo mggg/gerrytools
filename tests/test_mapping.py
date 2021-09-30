949ea165feb828957f358cf30520ddd40eeb7a50
apizzimenti <pizzimentianthony@gmail.com>
2021-09-30 18:46:06 -0400

    add testing for creating district maps

--- tests/test_mapping.py
+++ tests/test_mapping.py
@@ -1,5 +1,5 @@
 
-from plan_evaluation.mapping import drawgraph
+from plan_evaluation.mapping import drawgraph, drawplan
 from gerrychain.graph import Graph
 from pathlib import Path
 import matplotlib.pyplot as plt
@@ -13,15 +13,29 @@ def test_drawgraph():
     graph = Graph.from_json(root / "test-dualgraph.json")
     districts = gpd.read_file(root / "test-districts")
 
-    # Draw it!
-    figures, axes = drawgraph(graph, x="x", y="y")
-
     # Plot districts on the same axes.
-    ax = districts.plot(ax=axes[0], column="district")
+    ax = districts.plot(column="district")
 
-    plt.show()
+    # Draw it twice!
+    single_axes = drawgraph(graph, ax=ax, x="x", y="y")
+    multiple_axes = drawgraph(graph, components=True, x="x", y="y")
+
+    # Assert that single_axes is a single object, and that multiple_axes is a
+    # list of objects.
+    assert type(single_axes) is not list
+    assert type(multiple_axes) is list
+
+
+def test_drawplan():
+    # Read in a districting plan.
+    districts = gpd.read_file(root / "test-districts")
+    districts["district"] = districts["district"] + 1
+
+    # Draw the plan and make sure we get axes back.
+    ax = drawplan(districts, assignment="district", numbers=True)
+    assert ax is not None
 
 
 if __name__ == "__main__":
     root = Path(os.getcwd()) / Path("test_resources/")
-    test_drawgraph()
+    test_drawplan()
