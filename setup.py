e9b1aa3accd61f2a272bc65f6eb04eb2879946b5
apizzimenti <pizzimentianthony@gmail.com>
2021-10-11 11:42:11 -0400

    include subpackages

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 requirements = [
     "pandas", "scipy", "networkx", "geopandas", "shapely", "matplotlib",
@@ -12,6 +12,6 @@ setup(
     author_email="engineering@mggg.org",
     description="Tools for evaluating and visualizing districting plans.",
     url="https://github.com/mggg/plan-evaluation-tools",
-    packages=["evaltools"],
+    packages=find_packages(exclude=["tests"]),
     install_requires=requirements
 )
