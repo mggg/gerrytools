768d26a3f5f4626b6fa4f0dbfca4c6a42c49d276
apizzimenti <pizzimentianthony@gmail.com>
2022-01-24 15:01:23 -0500

    enforce .gitignore

--- tests/test-resources/test-assignments/profiling.sh
+++
@@ -1,4 +0,0 @@
-
-subdir="./test-resources/test-assignments/"
-gprof2dot -f pstats ${subdir}compress.pstats | dot -Tpng -o ${subdir}compress.png
-gprof2dot -f pstats ${subdir}decompress.pstats | dot -Tpng -o ${subdir}decompress.png
