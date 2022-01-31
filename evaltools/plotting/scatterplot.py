a39b2b70289d0ce174dc0062f127c0b0e9fa6524
Gabe Schoenbach <gabe.schoenbach@gmail.com>
2022-01-31 12:49:57 -0600

    clean up scatterplot

--- evaltools/plotting/scatterplot.py
+++ evaltools/plotting/scatterplot.py
@@ -1,7 +1,14 @@
 from matplotlib.axes import Axes
 import numpy as np
 
-def scatterplot(ax, x, y, labels, limits=set(), proposed_info={}, resolution=50, axis_range=None) -> Axes:
+def scatterplot(ax, 
+                x, 
+                y, 
+                labels=None, 
+                limits=set(), 
+                proposed_info={}, 
+                bins=None, 
+                axis_range=None) -> Axes:
     """
     Plot a scatterplot comparing two scores, with the proposed plans'
     scores as points.
@@ -10,7 +17,9 @@ def scatterplot(ax, x, y, labels, limits=set(), proposed_info={}, resolution=50,
         ax (Axes): `Axes` object on which the histogram is plotted.
         x (list): Score on the x-axis.
         y (list): score on the y-axis.
-        limits: (tuple, optional): x- and y-axis limits.
+        labels (list, optional): Strings for x- and y-axis labels.
+        limits (tuple, optional): Axis limits (specify to force plot to extend to 
+            these limits).
         proposed_info (dict, optional): Dictionary with keys of `colors`, `names`, 
             `x`, `y`; the \(i\)th color in `color` corresponds to the \(i\)th name 
             in `names`, which corresponds to the \(i\)th value in `x` and `y`.
@@ -18,37 +27,39 @@ def scatterplot(ax, x, y, labels, limits=set(), proposed_info={}, resolution=50,
     Returns:
         Axes object on which the scatterplot is plotted.
     """
-    min_val = min(min(x), min(y))
-    x_range = (min(x), max(x))
-    y_range = (min(y), max(y))
+    if not bins:
+        bins = [np.arange(int(min(x)), int(max(x)) + 1), np.arange(int(min(y)), int(max(y)) + 1)]
     h, xedges, yedges, image = ax.hist2d(x,
                                          y,
-                                         bins=[np.arange(min(x), max(x)+1, 10), np.arange(min(y), max(y)+1)],
+                                         bins=bins,
                                          cmap='Greys',
-                                        #  cmin=min_val,
                                          range=axis_range,
                                          )
+
+    ### Shift bins over by 0.5 to center labels in the middle of the bin.
+    ### TODO: This only works for bins of width 1 — need to fix for general bid width.
+    ax.set_xticks([x + 0.5 for x in xedges])
+    ax.set_xticklabels(xedges)
+    ax.set_yticks([y + 0.5 for y in yedges])
+    ax.set_yticklabels(yedges)
+
     if proposed_info:
         for i in range(len(proposed_info['names'])):
             x = proposed_info['x'][i]
             y = proposed_info['y'][i]
             ax.scatter(
                     x + 0.5,
-                    y,
-                    # label=f"{proposed_info['names'][i]} ({x}, {y})",
-                    label=f"{proposed_info['names'][i]}",
+                    y + 0.5,
+                    label=f"{proposed_info['names'][i]} ({x}, {y})",
                     color=proposed_info['colors'][i],
                     s=150,
                     edgecolor='black',
                    )
-    ax.legend()
-    xticklabels = ax.get_xticklabels()
-    xticks = ax.get_xticks()
-    
-    # ax.set_xticks([30.5, 35.5, 40.5, 45.5, 50.5])
-    # ax.set_xticklabels(["30", "35", "40", "45", "50"])
-    # ax.set_xlabel(labels[0], fontsize=20)
-    # ax.set_ylabel(labels[1], fontsize=20)
+        ax.legend()
+
+    if labels:
+        ax.set_xlabel(labels[0], fontsize=24)
+        ax.set_ylabel(labels[1], fontsize=24)
     if limits:
         ax.set_xlim(limits[0])
         ax.set_ylim(limits[1])
