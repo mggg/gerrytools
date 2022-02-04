76a337bab48d0007d61005e13b91682fd369a0df
apizzimenti <pizzimentianthony@gmail.com>
2022-02-04 15:35:58 -0500

    manage install and dev install dependencies

--- setup.py
+++ setup.py
@@ -14,5 +14,10 @@ setup(
     description="Tools for processing and visualizing districting plans.",
     url="https://github.com/mggg/plan-evaluation-processing",
     packages=find_packages(exclude=["tests"]),
-    install_requires=requirements
+    install_requires=requirements,
+    extras_require={
+        "dev": [
+            "pdoc3"
+        ]
+    }
 )
