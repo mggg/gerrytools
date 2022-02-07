e4f015c3dbf52ee82f8953f120cd60a97e976c30
apizzimenti <pizzimentianthony@gmail.com>
2022-02-07 16:05:23 -0500

    implement fixes to census API code; implement more robust set of tests

--- tests/test_data.py
+++ tests/test_data.py
@@ -103,35 +103,111 @@ def test_acs5_bgs():
 
 
 def test_census_tracts():
+    # Get a test set of data on Alabama.
     AL = us.states.AL
-    original = census(AL, geometry="tract", table="P4")
-    columns = {"GEOID20"} | set(variables("P4").values())
     tracts = 1437
-
-    assert len(original) == tracts
-    assert set(list(original)) == columns
-    
-    # Download additional variables and verify whether they match appropriately.
-    data = census(AL, table="P2", columns={"P2_003N": "NHISP"}, geometry="tract")
-    columns = {"GEOID20", "NHISP"}
-
-    assert len(data) == tracts
-    assert set(list(data)) == columns
-
-    # Now download *more* additional data and verify whether they match
-    # appropriately.
-    vars = variables("P3")
-    varnames = {
-        var: name
-        for var, name in vars.items() if "WHITE" in name or "BLACK" in name
+    tests = {
+        "P1": [
+            ("TOTPOP20", 5024279),
+            ("WHITEASIANPOP20", 18510)
+        ],
+        "P2": [
+            ("NHWHITEAMINASIANPOP20", 821),
+            ("HPOP20", 264047)
+        ],
+        "P3": [
+            ("WHITEASIANOTHVAP20", 201),
+            ("VAP20", 3917166)
+        ],
+        "P4": [
+            ("NHBLACKASIANOTHVAP20", 31),
+            ("HVAP20", 166856)
+        ]
     }
-    columns = {"GEOID20"} | set(varnames.values())
 
-    # Get the data.
-    data = census(AL, table="P3", columns=varnames, geometry="tract")
+    # Get the data for each table and verify that the values are correct.
+    for table, cases in tests.items():
+        data = census(AL, table=table, geometry="tract")
+        columns = set(variables(table).values()) | {"GEOID20"}
+        
+        # Assert we have the correct number of values and the correct columns.
+        assert len(data) == tracts
+        assert set(list(data)) == columns
 
-    assert len(data) == tracts
-    assert set(list(data)) == columns
+        # For each test case, confirm that we have the correct sum.
+        for column, correct in cases: assert data[column].sum() == correct
+
+
+def test_census_bgs():
+    # Get a test set of data on Alabama.
+    AL = us.states.AL
+    bgs = 3925
+    tests = {
+        "P1": [
+            ("TOTPOP20", 5024279),
+            ("WHITEASIANPOP20", 18510)
+        ],
+        "P2": [
+            ("NHWHITEAMINASIANPOP20", 821),
+            ("HPOP20", 264047)
+        ],
+        "P3": [
+            ("WHITEASIANOTHVAP20", 201),
+            ("VAP20", 3917166)
+        ],
+        "P4": [
+            ("NHBLACKASIANOTHVAP20", 31),
+            ("HVAP20", 166856)
+        ]
+    }
+
+    # Get the data for each table and verify that the values are correct.
+    for table, cases in tests.items():
+        data = census(AL, table=table, geometry="block group")
+        columns = set(variables(table).values()) | {"GEOID20"}
+        
+        # Assert we have the correct number of values and the correct columns.
+        assert len(data) == bgs
+        assert set(list(data)) == columns
+
+        # For each test case, confirm that we have the correct sum.
+        for column, correct in cases: assert data[column].sum() == correct
+
+
+def test_census_blocks():
+    AL = us.states.AL
+    blocks = 185976
+
+    tests = {
+        "P1": [
+            ("TOTPOP20", 5024279),
+            ("WHITEASIANPOP20", 18510)
+        ],
+        "P2": [
+            ("NHWHITEAMINASIANPOP20", 821),
+            ("HPOP20", 264047)
+        ],
+        "P3": [
+            ("WHITEASIANOTHVAP20", 201),
+            ("VAP20", 3917166)
+        ],
+        "P4": [
+            ("NHBLACKASIANOTHVAP20", 31),
+            ("HVAP20", 166856)
+        ]
+    }
+
+    # Get the data for each table and verify that the values are correct.
+    for table, cases in tests.items():
+        data = census(AL, table=table, geometry="block")
+        columns = set(variables(table).values()) | {"GEOID20"}
+        
+        # Assert we have the correct number of values and the correct columns.
+        assert len(data) == blocks
+        assert set(list(data)) == columns
+
+        # For each test case, confirm that we have the correct sum.
+        for column, correct in cases: assert data[column].sum() == correct
 
 def test_assignmentcompressor_compress():
     # Get the GEOIDs from the blocks.
@@ -303,5 +379,5 @@ def test_remap():
     plans = remap(plans, unitmaps)
 
 if __name__ == "__main__":
-    test_acs5_tracts()
+    test_census_tracts()
  
