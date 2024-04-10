6d388867d7fe8c1f9cb9f1ed3800c3d6cbc93d18
peterrrock2 <peter.r.rock2@gmail.com>
2024-04-10 07:58:01 -0600

    Fix build path for RTD

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,7 @@ try:
 except ImportError:
     import unittest.mock as mock
 
-sys.path.insert(0, os.path.abspath("../../gerrytools"))
+sys.path.insert(0, os.path.abspath("../../"))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
