f2fdedc3752a1e9597b4a3660d52bf2ce427fb97
peterrrock2 <27579114+peterrrock2@users.noreply.github.com>
2026-08-07 14:50:11 -0600

    add analytics to rtd

---
+++ user_guide/_static/js/traffic-tracking.js
@@ -0,0 +1,7 @@
+(() => {
+  const script = document.createElement("script");
+  script.src = "https://analytics.ds.uchicago.edu/script.js";
+  script.dataset.websiteId = "8a1e1999-058f-4b44-abc9-baf0318d81fb";
+  script.defer = true;
+  document.head.appendChild(script);
+})();
